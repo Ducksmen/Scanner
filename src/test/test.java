45bca50005449f4b5de2fe07cb326466a6433c84
BT_1N3_24 <jhoang6606@bths.edu>
2018-09-24 09:57:08 -0400

    Asks for age now.

--- src/test/test.java
+++ src/test/test.java
@@ -9,6 +9,9 @@ public class test {
         System.out.println("What is your name?");
         String name = input.nextLine();
         System.out.println("Hello " + name + ".");
+        System.out.println("How old are you?");
+        int age = input.nextInt();
+        System.out.println("Oh you are " + age + " years old, that's nice.");
         input.close();
     }
 }
